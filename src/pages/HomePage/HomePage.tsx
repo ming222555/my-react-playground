42102dfc070ef2ea8e935a6f27aab6c63f24dc91
ming222555 <ming222555@yahoo.com>
2020-10-25 15:26:03 +0800

    Del test scss files, remove @import and import stmts on test scss files. See sass/README.txt

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './HomePage.scss';
 
-// import 'sass/_testing.scss'; // werks
-import 'abab/_test.scss';    // werks
-
 function HomePage() {
     return (
         <div className="homePage">
