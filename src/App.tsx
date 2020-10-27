f1a32578b4c5936133a6bc6bd27cef7d4ea94b7a
ming222555 <ming222555@yahoo.com>
2020-10-27 11:43:14 +0800

    Verify CSS Modules on css, scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.scss';
+import s from './App.module.scss';
 import HomePage from 'pages/HomePage/HomePage';
 
 function App() {
     return (
-        <div className="app">
+        <div className={`${s.app}`}>
+            <span className="u-underline">I am App</span>
+            <div className={`${s.departments}`}>
+                App departments
+            </div>
             <HomePage />
         </div>
     );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './HomePage.scss';
+import s from './HomePage.module.scss';
 
 function HomePage() {
     return (
-        <div className="homePage">
+        <div className={`${[s.homePage, 'u-underline'].join(' ')}`}>
             I am HomePage
+            <div className={`${s.departments}`}>
+                HomePage departments
+            </div>
         </div>
     );
 }
